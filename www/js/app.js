a74b53b378978b037552d07296ef72ded893cc6a
ramsys <ramborghacklab@gmail.com>
2014-07-06 11:34:59 +0200

    Added Confirm popup for delete and modal for add new task

--- www/js/app.js
+++ www/js/app.js
@@ -47,12 +47,64 @@ app.config(function($stateProvider, $urlRouterProvider) {
 })
 
 // CONTROLLERS
-app.controller('TodosCtrl', function($scope, TodosService) {
+app.controller('TodosCtrl', function($scope, $ionicModal, TodosService) {
   $scope.todos = TodosService.todos
+  
+  $ionicModal.fromTemplateUrl('newTodo.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+  
+  //function to add items to the existing list
+  $scope.AddItem = function (data) {
+    $scope.todos.push({
+      title: data.newItem,
+      done: false
+    });
+    data.newItem = '';
+    $scope.closeModal();
+  };
+  
 })
 
-app.controller('TodoCtrl', function($scope, todo) {
+app.controller('TodoCtrl', function($scope, $ionicPopup, todo) {
   $scope.todo = todo
+  
+  // A confirm dialog
+   $scope.showConfirm = function() {
+     var confirmPopup = $ionicPopup.confirm({
+       title: 'Delete ToDo',
+       template: 'Are you sure you want to delete this ToDo?'
+     });
+     confirmPopup.then(function(res) {
+       if(res) {
+         console.log('You are sure');
+       } else {
+         console.log('You are not sure');
+       }
+     });
+   };
+   
 })
 
 // SERVICE 
